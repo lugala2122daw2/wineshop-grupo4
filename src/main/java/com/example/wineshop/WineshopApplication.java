c4de6fd03e818abb0e522184b092881a6911d49b
Luis garcia <15586286.clot@fje.edu>
2022-08-23 10:51:11 +0200

    Setup proyecto

---
+++ src/main/java/com/example/wineshop/WineshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.wineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineshopApplication.class, args);
+    }
+
+}
