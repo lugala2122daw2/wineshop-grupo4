3b8044675113cd650f598d7e6c53760e7d9f44bf
andb13 <andreibuhai@protonmail.com>
2022-08-23 12:49:25 +0200

    Añadir carpetas + region

---
+++ src/main/java/com/example/wineshop/repository/RegionRepository.java
@@ -0,0 +1,8 @@
+package com.example.wineshop.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegionRepository {
+    
+}
---
+++ src/main/java/com/example/wineshop/service/RegionService.java
@@ -0,0 +1,8 @@
+package com.example.wineshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegionService {
+    
+}
