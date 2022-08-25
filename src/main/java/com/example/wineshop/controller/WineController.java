a762c08fb16cbbec64c4a7a62b1f8d20e5420789
andb13 <andreibuhai@protonmail.com>
2022-08-25 13:39:26 +0200

    Add YearVal, changed WIne to notnulls, id contro

--- src/main/java/com/example/wineshop/controller/WineController.java
+++ src/main/java/com/example/wineshop/controller/WineController.java
@@ -1,18 +1,12 @@
 package com.example.wineshop.controller;
 
 import java.util.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.wineshop.entity.Wine;
 import com.example.wineshop.service.WineService;
-import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -35,6 +29,7 @@ public class WineController {
 
     @PostMapping("/api/wine")
     public ResponseEntity<Wine> createNewWine(@RequestBody Wine wine){
+        wine.setId(0);
         return new ResponseEntity<>(wineService.createWine(wine), HttpStatus.OK);
     }
 
